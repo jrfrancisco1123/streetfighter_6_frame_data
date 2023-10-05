c9111c03dc0cf971bbc5acef724a79669ca8fd1b
Remiro Francisco <jrfrancisco1123@yahoo.com>
2023-10-05 13:56:57 -0700

    added the damage attribute for each move for every playable character

--- flask_app/frame_data/aki.py
+++ flask_app/frame_data/aki.py
@@ -1,80 +1,80 @@
 normals = {
-        'lp': ['5', '5-6', '7', ['4', '-1']],
-        'mp': ['6', '6-8', '16', ['3', '-3']],
-        'hp': ['12', '12-14', '21', ['1', '-5']],
-        'lk': ['5', '5-7', '11', ['4', '-2']],
-        'mk': ['9', '9-11', '15', ['6', '-2']],
-        'hk': ['9', '9-12', '19', ['4', '-3']],
-        'clp': ['4', '4-5', '9', ['4', '-1']],
-        'cmp': ['8', '8-10', '21', ['1', '-3']],
-        'chp': ['10', '10-12', '28', ['D', '-8']],
-        'clk': ['5', '5-7', '8', ['3', '-2']],
-        'cmk': ['7', '7-9', '16', ['4', '-3']],
-        'chk': ['10', '10-14', '21', ['0', '-3']],
-        'jlp': ['5', '5-13', '3 frames after landing', ['', '']],
-        'jmp': ['7', '7-10', '3 frames after landing', ['', '']],
-        'jhp': ['11', '11-16', '3 frames after landing', ['', '']],
-        'jlk': ['5', '5-10', '3 frames after landing', ['', '']],
-        'jmk': ['7', '7-12', '3 frames after landing', ['', '']],
-        'jhk': ['8', '8-12', '3 frames after landing', ['', '']]
+        'lp': ['5', '5-6', '7', ['4', '-1'], '300'],
+        'mp': ['6', '6-8', '16', ['3', '-3'], '600'],
+        'hp': ['12', '12-14', '21', ['1', '-5'], '800'],
+        'lk': ['5', '5-7', '11', ['4', '-2'], '300'],
+        'mk': ['9', '9-11', '15', ['6', '-2'], '700'],
+        'hk': ['9', '9-12', '19', ['4', '-3'], '800'],
+        'clp': ['4', '4-5', '9', ['4', '-1'], '300'],
+        'cmp': ['8', '8-10', '21', ['1', '-3'], '600'],
+        'chp': ['10', '10-12', '28', ['D', '-8'], '900'],
+        'clk': ['5', '5-7', '8', ['3', '-2'], '200'],
+        'cmk': ['7', '7-9', '16', ['4', '-3'], '600'],
+        'chk': ['10', '10-14', '21', ['0', '-3'], '900'],
+        'jlp': ['5', '5-13', '3 frames after landing', ['', ''], '300'],
+        'jmp': ['7', '7-10', '3 frames after landing', ['', ''], '700'],
+        'jhp': ['11', '11-16', '3 frames after landing', ['', ''], '800'],
+        'jlk': ['5', '5-10', '3 frames after landing', ['', ''], '300'],
+        'jmk': ['7', '7-12', '3 frames after landing', ['', ''], '600'],
+        'jhk': ['8', '8-12', '3 frames after landing', ['', ''], '800']
 }
 
 uniques = {
-    'pu lao': ['24', '24-26', '18', ['3', '-2']],
-    'chi wen': ['16', '16-18', '21', ['63', '-4']],
-    'qiu niu': ['14', '14-24', '16', ['4', '2']],
-    'gong fu': ['9', '9-13', '3 frames after landing', ['D', '-15']],
-    'hun dun': ['8', '8-9', '14', ['0', '-3']],
-    'qiong qi': ['14', '14-16', '14', ['D', '-15']]
+    'pu lao': ['24', '24-26', '18', ['3', '-2'], '600'],
+    'chi wen': ['16', '16-18', '21', ['63', '-4'], '900'],
+    'qiu niu': ['14', '14-24', '16', ['4', '2'], '800'],
+    'gong fu': ['9', '9-13', '3 frames after landing', ['D', '-15'], '800'],
+    'hun dun': ['8', '8-9', '14', ['0', '-3'], '300'],
+    'qiong qi': ['14', '14-16', '14', ['D', '-15'], '400']
 }
 
 specials = {
-    'nightshade pulse': ['17', '', '52 total frames', ['-5', '-10']],
-    'nightshade chaser': ['13', '13-14', '34', ['-4', '-16']],
-    'od nightshade pulse': ['16', '', '44 total frames', ['D', '1']],
-    'od nightshade chaser': ['13', '13-17', '28', ['-4', '-13']],
-    'nightshade chaser (burst)': ['', '', '', ['D', '']],
-    'od nightshade chaser (burst)': ['', '', '', ['D', '']],
-    'orchid spring': ['26', '*', '46 total frames', ['', '']],
-    'toxic wreath': ['13', '13-36', '16', ['D', '-4']],
-    'light serpent lash': ['14', '14-16', '26', ['1', '-8']],
-    'medium serpent lash': ['17', '17-22', '24', ['D', '-12']],
-    'heavy serpent lash': ['11', '11-16', '32', ['D', '-20']],
-    'od serpent lash': ['21', '21-25', '25', ['4', '-14']],
-    'light cruel fate': ['24', '24-45', '18 frames after landing', ['1', '-3']],
-    'medium cruel fate': ['28', '28-49', '18 frames after landing', ['3', '-3']],
-    'heavy cruel fate': ['33', '33-54', '18 frames after landing', ['4', '-3']],
-    'od cruel fate': ['28', '28-49', '18 frames after landing', ['D', '2']],
-    'light snake step': ['', '', '37 total frames', ['', '']],
-    'medium snake step': ['', '', '39 total frames', ['', '']],
-    'heavy snake step': ['', '', '43 total frames', ['', '']],
-    'od snake step': ['', '', '42 total frames', ['', '']],
-    'sinister slide': ['', '', '216 total frames', ['', '']],
-    'venomous fang': ['19', '19-33', '30', ['D', '-25']],
-    'heel strike': ['11', '11-25', '20', ['4', '-3']],
-    'entrapment': ['23', '23-25', '55', ['D', '']]
+    'nightshade pulse': ['17', '', '52 total frames', ['-5', '-10'], '500'],
+    'nightshade chaser': ['13', '13-14', '34', ['-4', '-16'], '500'],
+    'od nightshade pulse': ['16', '', '44 total frames', ['D', '1'], '700'],
+    'od nightshade chaser': ['13', '13-17', '28', ['-4', '-13'], '600'],
+    'nightshade chaser (burst)': ['', '', '', ['D', ''], '800'],
+    'od nightshade chaser (burst)': ['', '', '', ['D', ''], '1600'],
+    'orchid spring': ['26', '*', '46 total frames', ['', ''], '0'],
+    'toxic wreath': ['13', '13-36', '16', ['D', '-4'], '800'],
+    'light serpent lash': ['14', '14-16', '26', ['1', '-8'], '500'],
+    'medium serpent lash': ['17', '17-22', '24', ['D', '-12'], '600'],
+    'heavy serpent lash': ['11', '11-16', '32', ['D', '-20'], '700'],
+    'od serpent lash': ['21', '21-25', '25', ['4', '-14'], '600'],
+    'light cruel fate': ['24', '24-45', '18 frames after landing', ['1', '-3'], '800'],
+    'medium cruel fate': ['28', '28-49', '18 frames after landing', ['3', '-3'], '900'],
+    'heavy cruel fate': ['33', '33-54', '18 frames after landing', ['4', '-3'], '1000'],
+    'od cruel fate': ['28', '28-49', '18 frames after landing', ['D', '2'], '1600'],
+    'light snake step': ['', '', '37 total frames', ['', ''], '0'],
+    'medium snake step': ['', '', '39 total frames', ['', ''], '0'],
+    'heavy snake step': ['', '', '43 total frames', ['', ''], '0'],
+    'od snake step': ['', '', '42 total frames', ['', ''], '0'],
+    'sinister slide': ['', '', '216 total frames', ['', ''], '0'],
+    'venomous fang': ['19', '19-33', '30', ['D', '-25'], '1100'],
+    'heel strike': ['11', '11-25', '20', ['4', '-3'], '600'],
+    'entrapment': ['23', '23-25', '55', ['D', ''], '1852']
 }
 
 super_arts = {
-    'deadly implication': ['10', '10-12', '68', ['D', '-46']],
-    'tainted talons': ['7', '*', '65', ['D', '-19']],
-    'claws of ya zi': ['10', '10-12', '58', ['D', '-36']],
-    'claws of ya zi (under 25% vit)': ['10', '10-12', '58', ['D', '-36']]
+    'deadly implication': ['10', '10-12', '68', ['D', '-46'], '1800'],
+    'tainted talons': ['7', '*', '65', ['D', '-19'], '2300'],
+    'claws of ya zi': ['10', '10-12', '58', ['D', '-36'], '4000'],
+    'claws of ya zi (under 25% vit)': ['10', '10-12', '58', ['D', '-36'], '4500']
 }
 
 throws = {
-    'whisper': ['5', '5-7', '23', ['D', '']],
-    'glutony': ['5', '5-7', '23', ['D', '']]
+    'whisper': ['5', '5-7', '23', ['D', ''], '1200'],
+    'glutony': ['5', '5-7', '23', ['D', ''], '1200']
 }
 
 commons = {
-    'forward dash': ['', '', '19 total frames', ['', '']],
-    'backward dash': ['', '', '23 total frames', ['', '']],
-    'drive impact': ['26', '26-27', '35', ['D', '-3']],
-    'drive reversal': ['20', '20-22', '26', ['D', '-8']],
-    'drive parry': ['', '', '30', ['', '']],
-    'perfect parry (strike)': ['1', '', '1', ['', '']],
-    'perfect parry (projectile)': ['1', '', '10', ['', '']],
-    'parry drive rush': ['', '', '45 total frames', ['', '']],
-    'cancel drive rush': ['', '', '46 total frames', ['', '']]
+    'forward dash': ['', '', '19 total frames', ['', ''], '0'],
+    'backward dash': ['', '', '23 total frames', ['', ''], '0'],
+    'drive impact': ['26', '26-27', '35', ['D', '-3'], '800'],
+    'drive reversal': ['20', '20-22', '26', ['D', '-8'], '500'],
+    'drive parry': ['', '', '30', ['', ''], '0'],
+    'perfect parry (strike)': ['1', '', '1', ['', ''], '0'],
+    'perfect parry (projectile)': ['1', '', '10', ['', ''], '0'],
+    'parry drive rush': ['', '', '45 total frames', ['', ''], '0'],
+    'cancel drive rush': ['', '', '46 total frames', ['', ''], '0']
 }
--- flask_app/frame_data/jp.py
+++ flask_app/frame_data/jp.py
@@ -1,85 +1,85 @@
 normals = {
-        'lp': ['6', '6-8', '10', ['4', '-2']],
-        'mp': ['12', '12-14', '21', ['1', '-6']],
-        'hp': ['12', '12-13', '22', ['3', '-3']],
-        'lk': ['5', '5-7', '11', ['3', '-2']],
-        'mk': ['8', '8-10', '19', ['1', '-3']],
-        'hk': ['12', '12-15', '17', ['7', '2']],
-        'clp': ['4', '4-5', '11', ['4', '-1']],
-        'cmp': ['7', '7-10', '14', ['6', '-2']],
-        'chp': ['9', '9-14', '20', ['1', '-6']],
-        'clk': ['6', '6-7', '10', ['2', '-2']],
-        'cmk': ['9', '9-11', '17', ['3', '-3']],
-        'chk': ['10', '10-12', '21', ['D', '-6']],
-        'jlp': ['5', '5-14', '3 frames after landing', ['', '']],
-        'jmp': ['12', '12-14', '3 frames after landing', ['', '']],
-        'jhp': ['9', '9-13', '3 frames after landing', ['', '']],
-        'jlk': ['6', '6-13', '3 frames after landing', ['', '']],
-        'jmk': ['7', '7-12', '3 frames after landing', ['', '']],
-        'jhk': ['11', '11-16', '3 frames after landing', ['', '']]
+        'lp': ['6', '6-8', '10', ['4', '-2'], '300'],
+        'mp': ['12', '12-14', '21', ['1', '-6'], '700'],
+        'hp': ['12', '12-13', '22', ['3', '-3'], '800'],
+        'lk': ['5', '5-7', '11', ['3', '-2'], '300'],
+        'mk': ['8', '8-10', '19', ['1', '-3'], '600'],
+        'hk': ['12', '12-15', '17', ['7', '2'], '800'],
+        'clp': ['4', '4-5', '11', ['4', '-1'], '300'],
+        'cmp': ['7', '7-10', '14', ['6', '-2'], '600'],
+        'chp': ['9', '9-14', '20', ['1', '-6'], '800'],
+        'clk': ['6', '6-7', '10', ['2', '-2'], '200'],
+        'cmk': ['9', '9-11', '17', ['3', '-3'], '700'],
+        'chk': ['10', '10-12', '21', ['D', '-6'], '900'],
+        'jlp': ['5', '5-14', '3 frames after landing', ['', ''], '300'],
+        'jmp': ['12', '12-14', '3 frames after landing', ['', ''], '700'],
+        'jhp': ['9', '9-13', '3 frames after landing', ['', ''], '800'],
+        'jlk': ['6', '6-13', '3 frames after landing', ['', ''], '300'],
+        'jmk': ['7', '7-12', '3 frames after landing', ['', ''], '600'],
+        'jhk': ['11', '11-16', '3 frames after landing', ['', ''], '800']
     }
 
 uniques = {
-    'guillotinna': ['22','22-23', '19', ['3', '-3']],
-    'malice': ['16', '16-18', '31', ['D', '-14']],
-    'bylina': ['11', '11-16', '24', ['D', '-5']],
-    'grom strelka (1)': ['8', '8-10', '16', ['5', '-1']],
-    'grom strelka (2)': ['10', '10-12', '20', ['3', '-6']],
-    'zilant': ['20', '20-22', '20', ['3', '-3']],
-    'zilant mid': ['21', '21-23', '22', ['3', '-4']],
-    'zilant low': ['21', '21-23', '22', ['3', '-4']]
+    'guillotinna': ['22','22-23', '19', ['3', '-3'], '700'],
+    'malice': ['16', '16-18', '31', ['D', '-14'], '900'],
+    'bylina': ['11', '11-16', '24', ['D', '-5'], '900'],
+    'grom strelka (1)': ['8', '8-10', '16', ['5', '-1'], '500'],
+    'grom strelka (2)': ['10', '10-12', '20', ['3', '-6'], '500'],
+    'zilant': ['20', '20-22', '20', ['3', '-3'], '500'],
+    'zilant mid': ['21', '21-23', '22', ['3', '-4'], '1000'],
+    'zilant low': ['21', '21-23', '22', ['3', '-4'], '1000']
 }
 
 specials = {
-    'light triglav': ['22', '22-31', '55 total frames', ['D', '-2']],
-    'medium triglav': ['22', '22-31', '55 total frames', ['D', '-2']],
-    'heavy triglav': ['22', '22-31', '55 total frames', ['D', '-2']],
-    'od light triglav': ['20', '20-39', '53 total frames', ['D', '3']],
-    'od medium triglav': ['20', '20-39', '53 total frames', ['D', '3']],
-    'od heavy triglav': ['20', '20-39', '53 total frames', ['D', '3']],
-    'light stribog': ['16', '16-21', '49 total frames', ['D', '-10']],
-    'medium stribog': ['20', '20-26', '53 total frames', ['D', '-8']],
-    'heavy stribog': ['28', '28-42', '61 total frames', ['D', '4']],
-    'od stribog': ['22', '22-33', '55 total frames', ['D', '2']],
-    'light departure': ['150', '150-159', '50 total frames', ['', '']],
-    'medium departure': ['150', '150-159', '50 total frames', ['', '']],
-    'heavy departure': ['150', '150-159', '50 total frames', ['', '']],
-    'od light departure': ['110', '110-149', '40 total frames', ['', '']],
-    'od medium departure': ['110', '110-149', '40 total frames', ['', '']],
-    'od heavy departure': ['110', '110-149', '40 total frames', ['', '']],
-    'departure window': ['', '', '44 total frames', ['', '']],
-    'departure shadow': ['', '', '40 total frames', ['', '']],
-    'amnesia': ['3', '3-20', '35', ['', '']],
-    'od amnesia': ['1', '1-20', '35', ['', '']],
-    'light torbalan': ['22', '', '50 total frames', ['D', '-6']],
-    'medium torbalan': ['26', '', '50 total frames', ['6', '-8']],
-    'heavy torbalan': ['26', '', '50 total frames', ['6', '-8']],
-    'od torbalan': ['22', '', '50 total frames', ['D', '25']],
-    'embrace': ['26', '', '72 total frames', ['', '']],
-    'od embrace': ['26', '', '72 total frames', ['', '']]
+    'light triglav': ['22', '22-31', '55 total frames', ['D', '-2'], '800'],
+    'medium triglav': ['22', '22-31', '55 total frames', ['D', '-2'], '800'],
+    'heavy triglav': ['22', '22-31', '55 total frames', ['D', '-2'], '800'],
+    'od light triglav': ['20', '20-39', '53 total frames', ['D', '3'], '1000'],
+    'od medium triglav': ['20', '20-39', '53 total frames', ['D', '3'], '1000'],
+    'od heavy triglav': ['20', '20-39', '53 total frames', ['D', '3'], '1000'],
+    'light stribog': ['16', '16-21', '49 total frames', ['D', '-10'], '1000'],
+    'medium stribog': ['20', '20-26', '53 total frames', ['D', '-8'], '1200'],
+    'heavy stribog': ['28', '28-42', '61 total frames', ['D', '4'], '800'],
+    'od stribog': ['22', '22-33', '55 total frames', ['D', '2'], '800'],
+    'light departure': ['150', '150-159', '50 total frames', ['', ''], '800'],
+    'medium departure': ['150', '150-159', '50 total frames', ['', ''], '800'],
+    'heavy departure': ['150', '150-159', '50 total frames', ['', ''], '800'],
+    'od light departure': ['110', '110-149', '40 total frames', ['', ''], '1000'],
+    'od medium departure': ['110', '110-149', '40 total frames', ['', ''], '1000'],
+    'od heavy departure': ['110', '110-149', '40 total frames', ['', ''], '1000'],
+    'departure window': ['', '', '44 total frames', ['', ''], '0'],
+    'departure shadow': ['', '', '40 total frames', ['', ''], '0'],
+    'amnesia': ['3', '3-20', '35', ['', ''], '800'],
+    'od amnesia': ['1', '1-20', '35', ['', ''], '1200'],
+    'light torbalan': ['22', '', '50 total frames', ['D', '-6'], '800'],
+    'medium torbalan': ['26', '', '50 total frames', ['6', '-8'], '1000'],
+    'heavy torbalan': ['26', '', '50 total frames', ['6', '-8'], '1000'],
+    'od torbalan': ['22', '', '50 total frames', ['D', '25'], '800'],
+    'embrace': ['26', '', '72 total frames', ['', ''], '1800'],
+    'od embrace': ['26', '', '72 total frames', ['', ''], '2600']
 }
 
 super_arts = {
-    'chornobog': ['8', '', '84 total frames', ['D', '-33']],
-    'lovushka': ['29', '', '8 total frames', ['', '']],
-    'interdiction': ['18', '18-23', '62', ['D', '-50']],
-    'interdiction (under 25% vit)': ['18', '18-23', '62', ['D', '-50']]
+    'chornobog': ['8', '', '84 total frames', ['D', '-33'], '2000'],
+    'lovushka': ['29', '', '8 total frames', ['', ''], '2000'],
+    'interdiction': ['18', '18-23', '62', ['D', '-50'], '4000'],
+    'interdiction (under 25% vit)': ['18', '18-23', '62', ['D', '-50'], '4500']
 }
 
 throws = {
-    'ravina': ['5', '5-7', '23', ['D', '']],
-    'uragan': ['5', '5-7', '23', ['D', '']],
-    'tornado (airborne)': ['5', '5-7', '3 frames after landing', ['D', '']]
+    'ravina': ['5', '5-7', '23', ['D', ''], '1200'],
+    'uragan': ['5', '5-7', '23', ['D', ''], '1200'],
+    'tornado (airborne)': ['5', '5-7', '3 frames after landing', ['D', ''], '1200']
 }
 
 commons = {
-    'forward dash': ['', '', '22 total frames', ['', '']],
-    'backward dash': ['', '', '23 total frames', ['', '']],
-    'drive impact': ['26', '26-27', '35', ['D', '-3']],
-    'drive reversal': ['20', '20-22', '26', ['D', '-8']],
-    'drive parry': ['1', '1-8', '29', ['', '']],
-    'perfect parry (strike)': ['1', '', '1', ['', '']],
-    'perfect parry (projectile)': ['1', '', '10', ['', '']],
-    'parry drive rush': ['', '', '45 total frames', ['', '']],
-    'cancel drive rush': ['', '', '46 total frames', ['', '']]
+    'forward dash': ['', '', '22 total frames', ['', ''], '0'],
+    'backward dash': ['', '', '23 total frames', ['', ''], '0'],
+    'drive impact': ['26', '26-27', '35', ['D', '-3'], '800'],
+    'drive reversal': ['20', '20-22', '26', ['D', '-8'], '500'],
+    'drive parry': ['1', '1-8', '29', ['', ''], '0'],
+    'perfect parry (strike)': ['1', '', '1', ['', ''], '0'],
+    'perfect parry (projectile)': ['1', '', '10', ['', ''], '0'],
+    'parry drive rush': ['', '', '45 total frames', ['', ''], '0'],
+    'cancel drive rush': ['', '', '46 total frames', ['', ''], '0']
 }
